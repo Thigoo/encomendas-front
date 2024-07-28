5edcfe60547072d905dd020082bd0fbce8617589
Thiago Garcia <90783156+Thigoo@users.noreply.github.com>
2024-07-27 23:02:52 -0300

    fix: error login validation (#6)
    * fix: error login validation

--- src/context/AuthContext/index.tsx
+++ src/context/AuthContext/index.tsx
@@ -1,29 +1,39 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { AuthContextType } from "../../models/AuthContextType";
 import { AuthProviderProps } from "../../models/AuthContextProps";
 import { login as apiLogin, fetchUserData } from "../../services/api";
 import { User } from "../../models/User";
 import { useNavigate } from "react-router-dom";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  user: User | null;
+  login: (user: User) => Promise<void>;
+  logout: () => void;
+}
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [user, setUser] = useState<User | null>(null);
-  const navigate = useNavigate();   
+  const navigate = useNavigate();
 
   const login = async (user: User) => {
     try {
-      const { token, userData } = await apiLogin(user);   
-      
+      const { token, userData } = await apiLogin(user);
+
       localStorage.setItem("token", token);
       setUser(userData);
       setIsAuthenticated(true);
 
-      navigate("/home");      
-    } catch (error) {
-      console.log("Erro ao fazer login", error);
-      throw error;
+      navigate("/home");
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        // toast.error('Email ou senha incorretos');
+        throw new Error("Email ou senha incorretos");
+      } else {
+        throw new Error("Erro ao realizar login");
+      }
     }
   };
 
@@ -32,21 +42,21 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     setUser(null);
     setIsAuthenticated(false);
   };
-  
+
   useEffect(() => {
-    const loadUserData = async() => {
+    const loadUserData = async () => {
       const token = localStorage.getItem("token");
-      if(token) {
+      if (token) {
         try {
           const userData = await fetchUserData(token);
           setUser(userData);
           setIsAuthenticated(true);
         } catch (error) {
-          console.error('Erro ao carregar os dados do usuário: ', error);
+          console.error("Erro ao carregar os dados do usuário: ", error);
           logout();
         }
       }
-    }
+    };
     loadUserData();
   }, []);
 
@@ -56,7 +66,6 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     </AuthContext.Provider>
   );
 };
-
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
